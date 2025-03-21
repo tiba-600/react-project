275292a887a9435f6233ac0848660321acdf36e6
Abbas Tiba <114922281+tiba-600@users.noreply.github.com>
2025-03-21 15:06:51 +0100

    fix

--- powerpros_pp_bv/vite.config.js
+++ powerpros_pp_bv/vite.config.js
@@ -1,20 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/react-project",
+  plugins: [react()], 
+  // base: process.env.VITE_BASE_PATH || "/react-project", 
+  base: process.env.VITE_BASE_PATH || "react-project",
   resolve: {
     alias: {
       "@design": path.resolve("./src/app/components/design"),
