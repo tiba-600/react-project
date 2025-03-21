7500b690348c0798b78125ba835f1ba9e727cf06
Abbas Tiba <114922281+tiba-600@users.noreply.github.com>
2025-03-21 18:15:20 +0100

    json

---
+++ powerpros_pp_bv/vite.config.js
@@ -0,0 +1,39 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()], 
+//   // base: process.env.VITE_BASE_PATH || "/react-project", 
+//   base: process.env.VITE_BASE_PATH || "/react-project",
+//   resolve: {
+//     alias: {
+//       "@design": path.resolve("./src/app/components/design"),
+//       "@functional": path.resolve("./src/app/components/functional"),
+//       "@core": path.resolve("./src/core"),
+//       // "@Modules": path.resolve("./src/app/core/Modules/categories"),
+//       "@pages": path.resolve("./src/app/pages"),
+//       "@style": path.resolve("./src/app/style"),
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/react-project",  
+  resolve: {
+    alias: {
+      "@design": path.resolve("./src/app/components/design"),
+      "@functional": path.resolve("./src/app/components/functional"),
+      "@core": path.resolve("./src/core"),
+      "@pages": path.resolve("./src/app/pages"),
+      "@style": path.resolve("./src/app/style"),
+    },
+  },
+});
+
